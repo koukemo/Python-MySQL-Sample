6266939bbf0ddbc2efc18efa2317f6f886102bd4
koukemo <java.sparrow22.surface@gmail.com>
2022-06-13 20:56:27 +0900

    [feat] Pythonから全テーブルを閲覧する機能とテーブル内データを削除する機能を追加

---
+++ src/delete.py
@@ -0,0 +1,9 @@
+from sql_source.sql_operation import Sqldelete
+
+
+def main():
+    Sqldelete.delete_all_table()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/show.py
@@ -0,0 +1,9 @@
+from sql_source.sql_operation import Sqlshow
+
+
+def main():
+    Sqlshow.show_all_tables()
+
+
+if __name__ == "__main__":
+    main()
